b7fc30a2b68bf2a36ae9b99c5b2512b36dd4b6d2
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-22 19:28:13 -0700

    Ran npm install, wrote basic endpoints, created new database

--- server.js
+++ server.js
@@ -3,5 +3,52 @@ const express = require('express');
 const server = express();
 
 // your code here
+const db = require('./data/accounts-model.js');
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  try {
+    const returnedData = await db.find();
+
+    res.send(returnedData);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+})
+
+server.post('/', async (req, res) => {
+  try {
+    const postData = req.body;
+    const returnedData = await db.add(postData);
+
+    res.status(201).send(returnedData);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+})
+
+server.put('/:id', async (req, res) => {
+  try {
+    const targetId = req.params.id;
+    const updateData = req.body;
+    const returnedData = await db.update(targetId, updateData);
+
+    res.send(returnedData);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+})
+
+server.delete('/:id', async (req, res) => {
+  try {
+    const targetId = req.params.id;
+    const returnedData = await db.remove(targetId);
+
+    res.send(returnedData);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+})
 
 module.exports = server;
